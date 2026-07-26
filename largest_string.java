6807ef106d6bd0f5862822eef9153dcdfadef5fa
MANISH YADAV <my5999555@gmail.com>
2026-07-26 12:09:11 +0530

    Create largest_string.java

---
+++ largest_string.java
@@ -0,0 +1,13 @@
+public class largest_string {
+    public static void main(String[] args) {
+        String fruit[] = {"apple", "mango" , "banana"};
+        String largest = fruit[0];
+        for(int i = 0 ; i<fruit.length ; i++){
+            if(largest.compareTo(fruit[i])<0){
+                largest = fruit[i];
+            }
+        } 
+        System.out.println(largest);
+    }
+    
+}
