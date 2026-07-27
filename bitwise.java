afeb2183f88ea1896390314f7ab919f6e9dd9957
MANISH YADAV <my5999555@gmail.com>
2026-07-28 01:43:47 +0530

    Create bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,22 @@
+public class bitwise {
+    public static void main(String[] args) {
+        // and operator
+        System.out.println(5&6);
+
+        // or operator
+        System.out.println(5|6);
+
+        //  xor operator
+        System.out.println(5^6);
+
+        // ones comp
+        System.out.println(~5);
+
+        //left shift
+        System.out.println(5<<2);
+
+        //right shift
+        System.out.println(6>>1);
+    }
+    
+}
