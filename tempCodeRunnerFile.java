e603dd327c1e0f2ea35cbb4b12b8fafa205e34c2
MANISH YADAV <my5999555@gmail.com>
2026-08-05 16:21:03 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,13 +1,1 @@
-public class fibnachhi {
-    public static int fib(int n ){
-        if(n==0 || n==1 ){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
-    }
 
-    public static void main(String[] args) {
-        int n = 5;
-        System.err.println(fib(n));
-    }
-}
