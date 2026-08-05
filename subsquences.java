877d14056bca908bbcca5fddf35334d085c17536
MANISH YADAV <my5999555@gmail.com>
2026-08-06 00:54:35 +0530

    Create subsquences.java

---
+++ subsquences.java
@@ -0,0 +1,19 @@
+public class subsquences {
+    public static void subsq(String str , int index , String newstr){
+        if(index == str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        char currchar = str.charAt(index);
+        // to be 
+        subsq(str, index+1, newstr+currchar);
+        // not to be
+
+        subsq(str, index+1, newstr);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        subsq(str, 0, "");
+
+    }
+}
