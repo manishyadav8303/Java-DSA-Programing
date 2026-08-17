4aa198453b5c89eb3e4223e19f06aa3b28b3517b
MANISH YADAV <my5999555@gmail.com>
2026-08-17 18:16:59 +0530

    Create mostfrequent.java

---
+++ mostfrequent.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class mostfrequent {
+    public ArrayList<Integer> mostfre(ArrayList<Integer>nums , int key){
+        int result[] = new int[1000];
+        for(int i = 0 ; i<nums.size()-1 ; i++){
+            
+        }
+
+    }
+}
