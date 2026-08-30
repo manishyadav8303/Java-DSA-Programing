7c3a3b88e480640eedd04257d071bdcf4b4827f6
MANISH YADAV <my5999555@gmail.com>
2026-08-30 11:17:32 +0530

    Create binarynoquesue.java

---
+++ binarynoquesue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class binarynoquesue {
+
+    public static void generate(int n) {
+
+        Queue<String> q = new LinkedList<>();
+
+        q.add("1");
+
+        for (int i = 1; i <= n; i++) {
+
+            String curr = q.remove();
+
+            System.out.print(curr + " ");
+
+            q.add(curr + "0");
+            q.add(curr + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        generate(5);
+    }
+}
