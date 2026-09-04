ba6461f6cb93bf8a12e78d7e905d1ffefca185d0
MANISH YADAV <my5999555@gmail.com>
2026-09-04 10:25:05 +0530

    Create countbinarytree.java

---
+++ countbinarytree.java
@@ -0,0 +1,52 @@
+
+
+public class countbinarytree {
+   
+    static class Node{
+        int data;
+        Node left;
+        Node right ;
+        Node(int data){
+            this.data = data;
+            this.left=null;
+            this.right =null;
+        }
+    }
+    public static int count(Node root){
+        
+        if(root == null){
+            return 0 ;
+        }
+        int lc = count(root.left);
+        int rc = count(root.right);
+        return lc+rc+1;
+    }
+     public static int sum(Node root){
+        
+        if(root == null){
+            return 0 ;
+        }
+        int ls = sum(root.left);
+        int rs = sum(root.right);
+        return ls+rs+root.data;
+    }
+        public static void main(String[] args) {
+            /*
+                        1
+                      /   \
+                    2      3
+                   / \    / \
+                  4   5      6 
+            */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+            System.out.println(sum(root));
+        }
+    
+}
+
