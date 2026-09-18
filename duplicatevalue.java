837be2bd89d6d578854b64cbae22e282d580164e
MANISH YADAV <my5999555@gmail.com>
2026-09-18 23:24:55 +0530

    Create duplicatevalue.java

---
+++ duplicatevalue.java
@@ -0,0 +1,11 @@
+public class duplicatevalue {
+    
+    public int singleNonDuplicate(int[] nums) {
+       int ans = 0;
+       for(int num:nums){
+       ans = ans^num;
+       } 
+       return ans;
+    }
+}
+
