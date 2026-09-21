5e69460ac4b6eed283176f6794e46b8d2754870f
MANISH YADAV <my5999555@gmail.com>
2026-09-21 23:07:35 +0530

    Update Lexicographically_smallest_string.java

--- Lexicographically_smallest_string.java
+++ Lexicographically_smallest_string.java
@@ -1,17 +1,41 @@
 public class Lexicographically_smallest_string {
-    public static String smallst(int n , int k){// k = sum
-        char ans[] = new char[n];
-        for(int i = 0 ; i<n ; i++){
-            ans[i] = "a";
-        }
-        int remain = k-n;
-          for (int i = n- 1; i >= 0; i--) {
 
-// ek character me hm max 25 add kr skhte hai
+    public static String smallest(int n, int k) {
+
+        char[] ans = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = 'a';
+        }
+
+        int remain = k - n;
+
+        for (int i = n - 1; i >= 0; i--) {
+
             int add = Math.min(remain, 25);
-            // like a+22 = w
+
             ans[i] = (char) (ans[i] + add);
 
-            remaining -= add;
+            remain -= add;
+        }
+
+        return new String(ans);
+    }
+    public static String smalle(int n , int k){
+        char ans[] = new char[n];
+        for(int i = 0 ; i<n ; i++){
+            ans[i]='a';
+        }
+        int remain = k-n;
+        for(int i = n-1 ; i>=0 ; i--){
+            int add = Math.min(remain, 25);
+            ans[i] = (char)(ans[i]+add);
+            remain -= add;
+        }
+        return new String(ans);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smalle(5, 73));
     }
 }
