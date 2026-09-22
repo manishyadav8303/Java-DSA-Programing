1ae01452588c83162a07d2d7e2c05d60cb159380
MANISH YADAV <my5999555@gmail.com>
2026-09-22 19:06:51 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,10 @@
+kaam
+        if (lastdsigit == 0) {
 
+            stringpair(n-1, 0, str+"0");
+            stringpair(n-1, 1, str+"1");
+            
+        }else{
+
+            stringpair(n-1, 0, str+"0");
+        }
